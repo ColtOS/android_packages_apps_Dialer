d8276ba5c7cda8079e17d1c53ce154ee55b2c111
GWX <kagwind@gmail.com>
2016-09-29 13:49:06 +0200

    [3/3] Flip to Mute/Reject Call
    Ported over from SlimKat implementation (thanks Kufi)
    
    Original message in 4.4:
    Ported over from 4.2 implementation (thanks sasikumardr for that)
        Original author of this patch was bugadani
        We did just a cleanup
        Provide easy mute and dismiss functionality for the phone
        application.
        Available actions:
        0: mute ringer
        1: dismiss call
        2: no action (default)
    
    Change-Id: If4ed9d388c47640482fdf693ac0cee85568b9467
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- InCallUI/src/com/android/incallui/InCallPresenter.java
+++ InCallUI/src/com/android/incallui/InCallPresenter.java
@@ -120,6 +120,7 @@ public class InCallPresenter implements CallList.Listener,
     private ExternalCallList mExternalCallList;
     private InCallActivity mInCallActivity;
     private InCallState mInCallState = InCallState.NO_CALLS;
+    private AccelerometerListener mAccelerometerListener;
     private ProximitySensor mProximitySensor;
     private boolean mServiceConnected = false;
     private boolean mAccountSelectionCancelled = false;
@@ -349,6 +350,8 @@ public class InCallPresenter implements CallList.Listener,
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK |
                 PowerManager.ACQUIRE_CAUSES_WAKEUP, "InCallPresenter");
+
+        mAccelerometerListener = new AccelerometerListener(context);
 
         mCallList = callList;
         mExternalCallList = externalCallList;
@@ -695,6 +698,10 @@ public class InCallPresenter implements CallList.Listener,
         Log.i(this, "Phone switching state: " + oldState + " -> " + newState);
         mInCallState = newState;
 
+        if (!newState.isIncoming() && mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(false);
+        }
+
         // notify listeners of new state
         for (InCallStateListener listener : mListeners) {
             Log.d(this, "Notify " + listener + " of state " + mInCallState.toString());
@@ -723,6 +730,10 @@ public class InCallPresenter implements CallList.Listener,
 
         Log.i(this, "Phone switching state: " + oldState + " -> " + newState);
         mInCallState = newState;
+
+        if (newState.isIncoming() && mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(true);
+        }
 
         for (IncomingCallListener listener : mIncomingCallListeners) {
             listener.onIncomingCall(oldState, mInCallState, call);
@@ -983,6 +994,10 @@ public class InCallPresenter implements CallList.Listener,
      * Answers any incoming call.
      */
     public void answerIncomingCall(Context context, int videoState) {
+        // Just in case
+        if (mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(false);
+        }
         // By the time we receive this intent, we could be shut down and call list
         // could be null.  Bail in those cases.
         if (mCallList == null) {
@@ -1006,6 +1021,10 @@ public class InCallPresenter implements CallList.Listener,
      * Declines any incoming call.
      */
     public void declineIncomingCall(Context context) {
+        // Just in case
+        if (mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(false);
+        }
         // By the time we receive this intent, we could be shut down and call list
         // could be null.  Bail in those cases.
         if (mCallList == null) {
@@ -1245,6 +1264,9 @@ public class InCallPresenter implements CallList.Listener,
         if (incomingCall != null) {
             TelecomAdapter.getInstance().answerCall(
                     incomingCall.getId(), VideoProfile.STATE_AUDIO_ONLY);
+            if (mAccelerometerListener != null) {
+                mAccelerometerListener.enableSensor(false);
+            }
             return true;
         }
 
@@ -1684,6 +1706,8 @@ public class InCallPresenter implements CallList.Listener,
                 mProximitySensor.tearDown();
             }
             mProximitySensor = null;
+
+            mAccelerometerListener = null;
 
             mWakeLock = null;
             mPowerManager = null;
